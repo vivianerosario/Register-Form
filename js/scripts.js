279fe924b14de3992356cef3d26b996eab42df4c
Viviane Rosário <vivianerosario.ep@gmail.com>
2021-08-16 13:12:41 -0300

    inclusion of fields: phone and cpf

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+class Validator {
+
+  constructor() {
+    this.validations = [
+      'data-min-length',
+      'data-max-length',
+      'data-only-letters',
+      'data-email-validate',
+      'data-required',
+      'data-equal',
+      'data-password-validate',
+    ]
+  }
